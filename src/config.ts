f23a4d3f1032c5124f6c21b0da41e59d34553ce9
cg10036 <cg10036@gmail.com>
2022-03-23 16:45:08 +0900

    add config

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export default {
+  spotifyOauth: "", // https://developer.spotify.com/console/post-playlists/?user_id=&body=%7B%22name%22%3A%22New%20Playlist%22%2C%22description%22%3A%22New%20playlist%20description%22%2C%22public%22%3Afalse%7D
+};
